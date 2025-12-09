3d3de1434314c2e85765ce69ab3583d4bc26b5d9
mayanbuilds <babaiayan90@gmail.com>
2025-12-09 13:50:17 +0500

    first commit

---
+++ Topics/JS Functions/app.js
@@ -0,0 +1,7 @@
+function greet(username){
+    consol.log("User Name is " + username);
+    
+
+}
+
+greet("Ayan Khan");
---
+++ Topics/JS If Else/app.js
@@ -0,0 +1,119 @@
+// Pthon File Remaining Questions 
+
+//  Q8;
+var result1=prompt("enter marks of subject 1");
+console.log(result1);
+var result2=prompt("enter marks of subject 2");
+console.log(result2);
+var result3=prompt("enter marks of subject 3");
+console.log(result3);
+if(result1>=40 && result2>=40 && result3>=40){
+    console.log("Pass")
+}else{
+    console.log("fail")
+}
+
+
+// Q9:
+var time = 7;
+if(time>=6 && time <= 17){
+    console.log("light mode")
+}else{
+    console.log("dark mode")
+}
+
+
+// Q12:
+var mealidentifier = 6;
+if(mealidentifier>=6 && mealidentifier<=10){
+    console.log("Breakfast")
+}else if(mealidentifier>=12 && mealidentifier<=14){
+    console.log("Lunch")
+}else if(mealidentifier>=18 && mealidentifier<=21){
+    console.log(Dinner)
+}else{console.log("Snack")}
+
+
+// Python File Remaining Questions Done!!!!
+
+// Q1:
+var age = 18;
+if(age>=18 && age<=100){
+    console.log("You are an Adult")
+}else if(age>=0 && age<=17){
+    console.log("you are a minor")
+}
+else{console.log("not eligible")}
+
+// Q2:
+var num = 18;
+if(num>=0 ){
+    console.log("positive")
+}else{
+    console.log("Negative")
+}
+
+// Q3:
+var num1 = 18;
+if(num1%2==0 ){
+    alert("positive number")
+}else{alert("odd number")}
+
+// Q4:
+var username = prompt("Enter Your Username")
+console.log("username")
+if(username=="admin"){
+    console.log("Welcome Admin")
+}else{
+    console.log("Access denied")
+}
+
+// Q5:
+var numA = 11;
+var numB = 12;
+if(numA==numB){
+    console.log("Both numbers are same")
+}else{
+    console.log("The numbers are different")
+}
+
+// Q6:
+var number1 = 1;
+var number2 = 12;
+if(number1>number2){
+    console.log("First is larger")
+}else{
+    console.log("Second is larger")
+}
+
+// Q7:
+var numbernew = 12;
+if(numbernew>=100){
+    console.log("Too big")
+}else{
+    console.log("within range")
+}
+
+// Q8:
+var score = 20;
+if(score>=50){
+    console.log("You Passed!")
+}else{
+    console.log("You Failed")
+}
+
+// Q9:
+var temp = 13;
+if(temp>=30){
+    console.log("It's Hot")
+}else{
+    console.log("It's Not Hot")
+}
+
+// Q10:
+var numbersecond = 0;
+if(numbersecond==0){
+    console.log("Zero")
+}else{
+    console.log("Not Zero")
+}
